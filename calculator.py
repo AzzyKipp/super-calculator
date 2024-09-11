f11cf52bad6a46133e7b18d91903968e76db567e
Isabella Urrego <isabellaurrego@gmail.com>
2024-09-11 09:09:45 -0500

    Menú inicial

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print ("Hola Usuario! En este programa podrás realizar las operaciones aritméticas básicas.")
+print("Presiona el número indicado según la operación uqe desees realizar")
+
+calculating = True
+
+while calculating:
+    print ("1. Sumar")
+    print ("2. Restar")
+    print ("3. Multiplicar ")
+    print ("4. Dividir")
+    print ("5. Salir")
+    
+    user_input = int(input())
