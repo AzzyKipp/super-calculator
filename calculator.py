da0d96527f3ea92722f2ce5b15116adc99c0aae4
Sebastián Pérez <sebastian.perezh2304@gmail.com>
2024-09-11 09:46:27 -0500

    Resta

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
 print ("Hola Usuario! En este programa podrás realizar las operaciones aritméticas básicas.")
-print("Presiona el número indicado según la operación uqe desees realizar")
+print("Presiona el número indicado según la operación que desees realizar")
 
 calculating = True
+
+def resta(n1, n2):
+    return n1 - n2
 
 while calculating:
     print ("1. Sumar")
@@ -11,3 +14,10 @@ while calculating:
     print ("5. Salir")
     
     user_input = int(input())
+    
+    if user_input == 2:
+        n1 = int(input("Escribe el primer número: "))
+        n2 = int(input("Escribe el número con el que deseas restar: "))
+        print(f"{n1} - {n2} = {resta(n1,n2)}")
+        break
+
