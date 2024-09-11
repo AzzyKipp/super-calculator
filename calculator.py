f0402a72efb673b35676f5e326f1097d97adaf09
Isabella Urrego <isabellaurrego@gmail.com>
2024-09-11 09:51:45 -0500

    Se agregó suma y resta

--- calculator.py
+++ calculator.py
@@ -3,6 +3,9 @@ print("Presiona el número indicado según la operación que desees realizar")
 
 calculating = True
 
+def suma(n1_suma,n2_suma):
+    return n1_suma + n2_suma
+
 def resta(n1, n2):
     return n1 - n2
 
@@ -14,8 +17,13 @@ while calculating:
     print ("5. Salir")
     
     user_input = int(input())
-    
-    if user_input == 2:
+    if user_input == 1:
+        n1_suma= int(input("Escribe el primer dígito a sumar: "))
+        n2_suma= int(input("Escribe el segundo dígito a sumar: "))
+        
+        print (n1_suma, "+", n2_suma, "=", suma(n1_suma,n2_suma))
+        break
+    elif user_input == 2:
         n1 = int(input("Escribe el primer número: "))
         n2 = int(input("Escribe el número con el que deseas restar: "))
         print(f"{n1} - {n2} = {resta(n1,n2)}")
